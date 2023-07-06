e06aca7fa32391cb2fde84f592f49a87015dc8b9
unknown <shroukz@yahoo.com>
2023-07-06 07:11:53 +0200

    done

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -16,7 +16,8 @@ export class SignupComponent  implements OnInit{
   registerForm: FormGroup = new FormGroup({first_name: new FormControl(null, [ Validators.required, Validators.minLength(2),Validators.maxLength(25), ]),
     last_name: new FormControl(null, [ Validators.required,  Validators.minLength(2),  Validators.maxLength(25),]),
     age: new FormControl(null, [Validators.required, Validators.min(10), Validators.max(80), ]),
-    email: new FormControl(null, [Validators.required, Validators.email]),password: new FormControl(null, [ Validators.required,  Validators.minLength(2), Validators.maxLength(10), Validators.pattern(/^[A-Z][\w]{1,10}$/),]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [ Validators.required,  Validators.minLength(2), Validators.maxLength(10), Validators.pattern(/^[A-Z][\w]{1,10}$/),]),
   });
   ngOnInit(): void {
   }
